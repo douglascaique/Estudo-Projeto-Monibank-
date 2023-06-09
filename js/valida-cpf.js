d5a709d87abe8ae275938ca51c90411f7c490d5a
douglascaique <60799530+douglascaique@users.noreply.github.com>
2023-06-09 02:13:52 -0300

    Mensagens de erros costumizadas em forms

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -2,9 +2,7 @@ export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
 
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse CPF não existe!")
-    } else {
-        console.log("CPF validado!")
+        campo.setCustomValidity('Esse CPF não é válido')
     }
 }
 
